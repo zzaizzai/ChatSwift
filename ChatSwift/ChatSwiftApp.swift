e7fac4b572602eff12627fcc5152690aeb65ac92
junsai <junsai0530@gmail.com>
2022-07-11 21:09:04 +0900

    Initial Commit

---
+++ ChatSwift/ChatSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatSwiftApp.swift
+//  ChatSwift
+//
+//  Created by 小暮準才 on 2022/07/11.
+//
+
+import SwiftUI
+
+@main
+struct ChatSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
